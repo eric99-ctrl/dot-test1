1f543511f776ef788893b1c9af68545d104add96
eric <repository.eric@gmail.com>
2022-05-25 21:54:25 +0700

    update:
    * final progress

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\APIController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/search/provinces', [APIController::class, 'searchProvince'])->name('province.search');
+Route::get('/search/cities', [APIController::class, 'searchCity'])->name('city.search');
+Route::get('/province/import', [APIController::class, 'importProvince'])->name('province.import');
+Route::get('/city/import', [APIController::class, 'importCity'])->name('city.import');
